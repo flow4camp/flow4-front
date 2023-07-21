722963b3ffcd52c181a41d62bc70fc786d651023
YinaePark <dlsoso1@unist.ac.kr>
2023-07-22 02:53:51 +0900

    feat: home 레이아웃

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,98 @@
+import React, { useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import EditCharacter from "./EditCharacter";
+
 function Home() {
-  return <div>home</div>;
+  const nickName = "user1";
+  const winNum = 1;
+  const looseNum = 12;
+  const dailyQuestList = [
+    { id: 1, text: "첫 번째 퀘스트" },
+    { id: 2, text: "두 번째 퀘스트" },
+    { id: 3, text: "세 번째 퀘스트" },
+  ];
+  const specialQuest = "특별 퀘스트";
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.userinfoContainer}>
+        <div style={styles.userinfoChar}>
+          <Link to="/edit-character">
+            <img src="/images/test_img.gif" />
+          </Link>
+        </div>
+        <div style={styles.userinfoText}>
+          <div style={styles.userName}>{nickName}</div>
+          <div>
+            전적 {winNum}승 {looseNum}패
+          </div>
+        </div>
+      </div>
+      <div style={styles.questContainer}>
+        <div style={styles.questType}>일일 퀘스트</div>
+        <div style={styles.quest}>
+          {dailyQuestList.map((quest) => (
+            <div key={quest.id}>{quest.text}</div>
+          ))}
+        </div>
+        <div style={styles.questType}>특별 퀘스트</div>
+        <div style={styles.quest}>{specialQuest}</div>
+      </div>
+      <Routes>
+        <Route path="/edit-character" element={<EditCharacter />} />
+      </Routes>
+    </div>
+  );
 }
+
+interface Styles {
+  container: React.CSSProperties;
+  userinfoContainer: React.CSSProperties;
+  questContainer: React.CSSProperties;
+  userinfoText: React.CSSProperties;
+  userinfoChar: React.CSSProperties;
+  questType: React.CSSProperties;
+  userName: React.CSSProperties;
+  quest: React.CSSProperties;
+}
+
+const styles: Styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    height: "100vh",
+  },
+  userinfoContainer: {
+    marginTop: "5%",
+    backgroundColor: "#EBEEF3",
+    borderRadius: "30px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "30%",
+  },
+  // 좌측 이미지 크기 제외한 나머지 크기 차지
+  userinfoText: { flex: 1 },
+  userinfoChar: {},
+  questContainer: {
+    borderTop: "1px solid #ccc",
+    marginTop: "13%",
+    flex: 1,
+  },
+  questType: {
+    marginTop: "4%",
+    fontSize: "23px",
+    fontWeight: "500",
+  },
+  userName: {
+    fontSize: "40px",
+    fontWeight: "600",
+  },
+  quest: {
+    borderBottom: "0.5px solid #ccc",
+    paddingBottom: "7%",
+  },
+};
 
 export default Home;
