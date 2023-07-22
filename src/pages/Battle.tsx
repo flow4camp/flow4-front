06cb5865581727e10f371e3d2981cbb4a87f677c
ryuihchae <ihchaeryu@kaist.ac.kr>
2023-07-22 19:57:22 +0900

    feat) 호선 click시 modal window

--- src/pages/Battle.tsx
+++ src/pages/Battle.tsx
@@ -1,14 +1,27 @@
 import NavBar from "../components/NavBar";
-import { Center, Image, Text, Box, Spacer, Flex } from '@chakra-ui/react';
+import { Center, Button, Text, Box, Spacer, Flex } from '@chakra-ui/react';
 import Layout from '../components/layout';
-
+import {
+  Modal,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ModalCloseButton,
+  ModalBody,
+} from '@chakra-ui/modal';
+import { useDisclosure } from '@chakra-ui/hooks';
 
 function Battle() {
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const openModalForMatch = () => {
+    onOpen();
+  }
+
   return (
     <Layout>
-    <Flex
-      direction='column'
-    >
       <Flex
         direction='column'
         w='100%'
@@ -35,7 +48,9 @@ function Battle() {
           <Flex
             direction='row'
             w='100%'
+            onClick={openModalForMatch}
           >
+            <Spacer />
             <Text p={2} alignSelf='center' style={{ fontFamily: 'Font-Content' }} >1호선</Text>
             <Spacer />
             <Box bg='blue' w='250px' h='100px' justifySelf='right'></Box>
@@ -43,7 +58,9 @@ function Battle() {
           <Flex
             direction='row'
             w='100%'
+            onClick={openModalForMatch}
           >
+            <Spacer />
             <Text p={2} alignSelf='center' style={{ fontFamily: 'Font-Content' }} >2호선</Text>
             <Spacer />
             <Box bg='green' w='250px' h='100px' justifySelf='right'></Box>
@@ -51,7 +68,9 @@ function Battle() {
           <Flex
             direction='row'
             w='100%'
+            onClick={openModalForMatch}
           >
+            <Spacer />
             <Text p={2} alignSelf='center' style={{ fontFamily: 'Font-Content' }} >3호선</Text>
             <Spacer />
             <Box bg='orange' w='250px' h='100px' justifySelf='right'></Box>
@@ -59,7 +78,9 @@ function Battle() {
           <Flex
             direction='row'
             w='100%'
+            onClick={openModalForMatch}
           >
+            <Spacer />
             <Text p={2} alignSelf='center' style={{ fontFamily: 'Font-Content' }} >4호선</Text>
             <Spacer />
             <Box bg='aqua' w='250px' h='100px' justifySelf='right'></Box>
@@ -67,7 +88,9 @@ function Battle() {
           <Flex
             direction='row'
             w='100%'
+            onClick={openModalForMatch}
           >
+            <Spacer />
             <Text p={2} alignSelf='center' style={{ fontFamily: 'Font-Content' }} >5호선</Text>
             <Spacer />
             <Box bg='purple' w='250px' h='100px' justifySelf='right'></Box>
@@ -75,15 +98,32 @@ function Battle() {
           <Flex
             direction='row'
             w='100%'
+            onClick={openModalForMatch}
           >
+            <Spacer />
             <Text p={2} alignSelf='center' style={{ fontFamily: 'Font-Content' }} >6호선</Text>
             <Spacer />
             <Box bg='brown' w='250px' h='100px' justifySelf='right'></Box>
           </Flex>
         </Flex>
+        <Modal
+          closeOnOverlayClick={false}
+          isOpen={isOpen}
+          onClose={onClose}
+          size='sm'
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Header</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>Body</ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>취소</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Flex>
-      </Flex>
-      </Layout>
+    </Layout>
   );
 }
 
