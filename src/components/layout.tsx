123c350f47a98bee4e9f4b9c1ddad92d975cbd36
yeono000 <tata0430@hanyang.ac.kr>
2023-07-22 19:31:47 +0900

    fix) layout scroll

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar';
 function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div style={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column' }}>
-            <div style={{flex:1}}>{ children }</div>
+            <div style={{flex:1, overflowY:'auto'}}>{ children }</div>
             <NavBar/>
         </div>
     );
