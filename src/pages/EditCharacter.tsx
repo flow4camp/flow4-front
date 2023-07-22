f1f4df75bcee751360ef2decdb20c1dcff1c00b0
YinaePark <dlsoso1@unist.ac.kr>
2023-07-22 19:40:31 +0900

    feat) inven grid

--- src/pages/EditCharacter.tsx
+++ src/pages/EditCharacter.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { background, useTheme } from "@chakra-ui/react";
 
 function EditCharacter() {
   const [selectedItem, setSelectedItem] = useState<string>("item1");
@@ -6,45 +7,69 @@ function EditCharacter() {
   const handleTabClick = (item: string) => {
     setSelectedItem(item);
   };
+  const theme = useTheme();
+  const itemNumbers = Array.from({ length: 36 }, (_, index) => index + 1);
 
   return (
     <div style={styles.container}>
-      <div style={styles.charContainer}></div>
+      <div
+        style={{
+          height: "50%",
+          backgroundColor: theme.colors.ziha_charcoal,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={require("../assets/ghost-basic-0.png")}
+          style={{
+            width: "300px",
+            height: "300px",
+            objectFit: "cover",
+          }}
+        />
+      </div>
       <div style={styles.tabContainer}>
         <button
           style={selectedItem === "clothes" ? styles.activeTab : styles.tab}
           onClick={() => handleTabClick("clothes")}
         >
-          <img src="/icons/clothes.png" style={{ width: "25px" }} />
+          <img src="/icons/clothes.png" style={{ width: "30px" }} />
         </button>
         <button
           style={selectedItem === "eyeglasses" ? styles.activeTab : styles.tab}
           onClick={() => handleTabClick("eyeglasses")}
         >
-          <img src="/icons/eyeglasses.png" style={{ width: "25px" }} />
+          <img src="/icons/eyeglasses.png" style={{ width: "30px" }} />
         </button>
         <button
           style={selectedItem === "hat" ? styles.activeTab : styles.tab}
           onClick={() => handleTabClick("hat")}
         >
-          <img src="/icons/hat.png" style={{ width: "25px" }} />
+          <img src="/icons/hat.png" style={{ width: "30px" }} />
         </button>
         <button
           style={selectedItem === "shoes" ? styles.activeTab : styles.tab}
           onClick={() => handleTabClick("shoes")}
         >
-          <img src="/icons/shoes.png" style={{ width: "25px" }} />
+          <img src="/icons/shoes.png" style={{ width: "30px" }} />
         </button>
         <button
           style={selectedItem === "ghost" ? styles.activeTab : styles.tab}
           onClick={() => handleTabClick("ghost")}
         >
-          <img src="/icons/ghost.png" style={{ width: "25px" }} />
+          <img src="/icons/ghost.png" style={{ width: "30px" }} />
         </button>
       </div>
-      <div style={styles.itemContainer}>
-        {selectedItem === "item1" && <div>Content for Item 1</div>}
-        {selectedItem === "item2" && <div>Content for Item 2</div>}
+      <div style={styles.itemContainerWrapper}>
+        <div style={styles.itemContainer}>
+          {itemNumbers.map((itemNumber) => (
+            <div key={itemNumber} style={styles.item}>
+              {itemNumber}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -57,11 +82,16 @@ interface Styles {
   tab: React.CSSProperties;
   activeTab: React.CSSProperties;
   itemContainer: React.CSSProperties;
+  item: React.CSSProperties;
+  itemContainerWrapper: React.CSSProperties;
 }
 
 const styles: Styles = {
   container: {
     height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
   },
   charContainer: {
     height: "50%",
@@ -69,8 +99,6 @@ const styles: Styles = {
   tabContainer: {
     display: "flex",
     alignItems: "center",
-    // padding: "10px",
-    borderBottom: "1px solid #ccc",
   },
   tab: {
     padding: "8px 16px",
@@ -86,7 +114,25 @@ const styles: Styles = {
     border: "none",
     background: "#ccc",
   },
-  itemContainer: {},
+  itemContainerWrapper: {
+    overflowY: "auto",
+  },
+  itemContainer: {
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gridTemplateRows: "repeat(4, 1fr)",
+    rowGap: "1%",
+    justifyContent: "center",
+    justifyItems: "center",
+  },
+  item: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80px",
+    height: "80px",
+    backgroundColor: "green",
+  },
 };
 
 export default EditCharacter;
