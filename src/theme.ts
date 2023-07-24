501731ac3fe8f2878eff34da895b940e10f5ae9a
YinaePark <dlsoso1@unist.ac.kr>
2023-07-24 15:50:24 +0900

    chore: theme.ts 오타 수정

--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,20 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
-//   brand: {
-//     900: '#1a365d',
-//     800: '#153e75',
-//     700: '#2a69ac',
-//   },
-    ziha_charcoal: '#323232',
-    ziha_charcoal_gray: '#5A5A5A',
-    ziha_green:'#CDF082',
-    ziha_green_gray: '#C3DB90',
-    ziha_purple: '#B1A6D2',
-    ziha_purple_gray: 'B9B3CA'
-}
+  //   brand: {
+  //     900: '#1a365d',
+  //     800: '#153e75',
+  //     700: '#2a69ac',
+  //   },
+  ziha_charcoal: "#323232",
+  ziha_charcoal_gray: "#5A5A5A",
+  ziha_green: "#CDF082",
+  ziha_green_gray: "#C3DB90",
+  ziha_purple: "#B1A6D2",
+  ziha_purple_gray: "#B9B3CA",
+};
 
-const theme = extendTheme({ colors })
+const theme = extendTheme({ colors });
 
 export default theme;
