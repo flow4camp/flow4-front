8e03b5c871644860ae1452f03156e05e48407f26
yeono000 <tata0430@hanyang.ac.kr>
2023-07-24 14:02:16 +0900

    feat) store ui

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -8,11 +8,15 @@ import { Center, Image, Text, Box, Spacer, Flex } from '@chakra-ui/react';
 const Store = () => {
   // 상태(State) 설정
   const [activeTab, setActiveTab] = useState('tab1'); // 기본적으로 첫 번째 탭이 선택되도록 설정
+  const [selectedItem, setSelectedItem] = useState<string>("item1");
   const theme = useTheme();
 
   // 탭 변경 핸들러 함수
   const handleTabChange = (tab :string) => {
     setActiveTab(tab);
+  };
+  const handleItemTabChange = (item: string) => {
+    setSelectedItem(item);
   };
 
   // 아이템 리스트 데이터 (여기서는 간단하게 배열로 구성)
@@ -41,16 +45,44 @@ const Store = () => {
         {/* 선택된 탭에 해당하는 아이템 리스트 렌더링 */}
         {activeTab === 'tab1' && (
           <div className='contents'>
-            {/* <ul>
-              {tab1Items.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul> */}
-          {
+          {<div>
+            <div style={styles.tabContainer}>
+              <button
+                style={selectedItem === "clothes" ? styles.activeTab : styles.tab}
+                onClick={() => handleItemTabChange("clothes")}
+              >
+                <img src="/icons/clothes.png" style={{ width: "30px" }} />
+              </button>
+              <button
+                style={selectedItem === "eyeglasses" ? styles.activeTab : styles.tab}
+                onClick={() => handleItemTabChange("eyeglasses")}
+              >
+                <img src="/icons/eyeglasses.png" style={{ width: "30px" }} />
+              </button>
+              <button
+                style={selectedItem === "hat" ? styles.activeTab : styles.tab}
+                onClick={() => handleItemTabChange("hat")}
+              >
+                <img src="/icons/hat.png" style={{ width: "30px" }} />
+              </button>
+              <button
+                style={selectedItem === "shoes" ? styles.activeTab : styles.tab}
+                onClick={() => handleItemTabChange("shoes")}
+              >
+                <img src="/icons/shoes.png" style={{ width: "30px" }} />
+              </button>
+              <button
+                style={selectedItem === "ghost" ? styles.activeTab : styles.tab}
+                onClick={() => handleItemTabChange("ghost")}
+              >
+                <img src="/icons/ghost.png" style={{ width: "30px" }} />
+              </button>
+            </div>
             <div className="grid-container">
               <div className="grid-item">Item 1</div>
               <div className="grid-item">Item 2</div>
               <div className="grid-item">Item 3</div>
+            </div>
             </div>
           }
           </div>
@@ -58,11 +90,6 @@ const Store = () => {
 
         {activeTab === 'tab2' && (
           <div className='contents'>
-            {/* <ul>
-            {tab2Items.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-            </ul> */}
                     {
             <div className="one-grid-container">
               <div className="one-grid-item">Item 1</div>
@@ -73,6 +100,47 @@ const Store = () => {
         )}
       </Layout>
   );
+};
+
+interface Styles {
+  container: React.CSSProperties;
+  charContainer: React.CSSProperties;
+  tabContainer: React.CSSProperties;
+  tab: React.CSSProperties;
+  activeTab: React.CSSProperties;
+}
+
+const styles: Styles = {
+  container: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
+  },
+  charContainer: {
+    height: "50%",
+  },
+  tabContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "100%",
+  },
+  tab: {
+    padding: "8px 16px",
+    margin: "0 5px",
+    cursor: "pointer",
+    border: "none",
+    background: "transparent",
+  },
+  activeTab: {
+    padding: "8px 16px",
+    margin: "0 5px",
+    cursor: "pointer",
+    border: "none",
+    borderRadius: "14px",
+    background: "#ccc",
+  },
 };
 
 export default Store;
