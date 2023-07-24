72b50e63be3cf7d4d69e97b86749ee915b1fef70
YinaePark <dlsoso1@unist.ac.kr>
2023-07-24 16:32:28 +0900

    feat: 지하철 노선 레이아웃 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -77,15 +77,80 @@ function MyPage() {
           <Button style={{ backgroundColor: "transparent" }}>
             <Image src="/icons/edit.png" w="10px" />
           </Button>
-          <div
+          <Flex
+            direction="column"
+            align="center"
             style={{
               borderRadius: "20px",
               height: "150px",
               width: "80%",
               marginTop: "10px",
               backgroundColor: theme.colors.ziha_charcoal,
+              position: "relative",
             }}
-          ></div>
+          >
+            <Text style={{ fontFamily: "Font-title-light", color: "white" }}>
+              Line 2
+            </Text>
+            <Box
+              style={{
+                width: "70%",
+                height: "60%",
+                border: "1.5px solid white",
+                borderRadius: "20px",
+              }}
+            ></Box>
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "15%",
+                transform: "translate(-50%, -50%)",
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%",
+                backgroundColor: "red",
+              }}
+            ></div>
+            <Text
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "15%",
+                transform: "translate(-50%, 50%)",
+                fontFamily: "Font-title-light",
+                color: "white",
+                fontSize: "10px",
+              }}
+            >
+              대전역
+            </Text>
+            <div
+              style={{
+                position: "absolute",
+                top: "50%", // Box 컴포넌트의 중앙에 위치하도록 설정
+                right: "15%", // 우측에 위치
+                transform: "translate(50%, -50%)", // 수직으로 정확히 중앙으로 위치시키기 위해 사용
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%", // 원형으로 만들기 위해 50%의 borderRadius 사용
+                backgroundColor: "blue", // 예시로 파란색 배경을 적용
+              }}
+            ></div>
+            <Text
+              style={{
+                position: "absolute",
+                top: "50%",
+                right: "15%",
+                transform: "translate(50%, 50%)",
+                fontFamily: "Font-title-light",
+                color: "white",
+                fontSize: "10px",
+              }}
+            >
+              유성온천역
+            </Text>
+          </Flex>
         </Flex>
         <Flex direction="column" style={{ width: "80%", marginTop: "50px" }}>
           <Button
