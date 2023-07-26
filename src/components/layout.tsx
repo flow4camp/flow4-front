00e3d834c2e8f02c9e58132821ea514ed0f124ad
yeono000 <tata0430@hanyang.ac.kr>
2023-07-26 19:31:23 +0900

    hotfix: crop

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ function Layout({ scroll, children }: { scroll? :string, children: React.ReactNo
     return (
         <div style={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column' }}>
             {   scroll === 'none' ?
-                <div style={{ flex: 1, display: 'flex', flexDirection:'column'}}>{children}</div>
+                <div style={{ flex: 1, display: 'flex', flexDirection:'column', overflowY:'clip'}}>{children}</div>
                 : <div style={{ flex: 1, overflowY: 'auto' }}>{children}</div>
             }
             <NavBar/>
