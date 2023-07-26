a239733397600bfee08687fc17ccf3a8bc1d9d61
ryuihchae <ihchaeryu@kaist.ac.kr>
2023-07-26 19:46:52 +0900

    feat) gameover component 완료

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Center, Button, Text, Box, Spacer, Flex, Image, Spinner } from "@chakra-ui/react";
+import UserCharacter from "../components/UserCharacter";
+import { storeFaceVariants } from "../components/StoreVariants";
 
 const GameOverWin = () => {
     return (
@@ -11,9 +13,18 @@ const GameOverWin = () => {
 };
 
 const GameOverLose = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <Center w='100%' h='100vh' p={3}>
-            <Flex direction='column'>
+        <Center w='100%' h='100vh' p={3} bg='ziha_charcoal' >
+            <Flex 
+                direction='column' 
+                bg='gray.300' 
+                p={10} 
+                borderRadius='3em'
+                boxShadow='0px 2px 6px rgba(255, 255, 255, 0.5)'
+            >
                 <Flex direction='column' >
                     <Text style={{ fontFamily: 'Font-Title'}} fontSize='xl'
                     >
@@ -25,8 +36,33 @@ const GameOverLose = () => {
                         <Image src={require('../assets/fight_gray.png')} w={8} h={8} />
                     </Flex>
                 </Flex>
-                <Flex direction='column'>
-                    
+                <Flex direction='column' >
+                    <Box w='200px' h='230px' justifySelf='center' alignSelf='center' >
+                        <UserCharacter
+                        usage={''}
+                        selectedHat={null}
+                        selectedAcc={null}
+                        selectedFace={storeFaceVariants[2]}
+                        selectedClothes={null}
+                        selectedShoe={null}
+                        />
+                    </Box>
+                </Flex>
+                <Flex direction='row' align='center' justify='center'>
+                    <Image src={require('../assets/coin.png')} w={10} h={10} />
+                    <Text style={{ fontFamily: 'Font-Title'}} fontSize='2xl' >- 20</Text>
+                </Flex>
+                <Flex justify='center' paddingTop={10}>
+                    <Button 
+                        style={{ fontFamily: 'Font-Content-Light'}}
+                        bg='ziha_purple_sharp'
+                        color='ziha_charcoal'
+                        borderRadius='0.8em'
+                        boxShadow='0px 2px 4px rgba(0, 0, 0, 0.2)'
+                        onClick={() => navigate('/home')}
+                    >
+                        메인으로 돌아가기
+                    </Button>
                 </Flex>
             </Flex>
         </Center>
