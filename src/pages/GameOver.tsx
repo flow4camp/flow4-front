8d75126991b4a2c6437c2e71d77586c9dff4cc5d
ryuihchae <ihchaeryu@kaist.ac.kr>
2023-07-26 19:04:40 +0900

    feat) lose component wip

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,5 +1,37 @@
 import React from "react";
 import { useLocation } from 'react-router-dom';
+import { Center, Button, Text, Box, Spacer, Flex, Image, Spinner } from "@chakra-ui/react";
+
+const GameOverWin = () => {
+    return (
+        <Center>
+
+        </Center>
+    );
+};
+
+const GameOverLose = () => {
+    return (
+        <Center w='100%' h='100vh' p={3}>
+            <Flex direction='column'>
+                <Flex direction='column' >
+                    <Text style={{ fontFamily: 'Font-Title'}} fontSize='xl'
+                    >
+                        GAME OVER</Text>
+                    <Flex direction='row' gap={3} align='center'>
+                        <Image src={require('../assets/fight_gray.png')} w={8} h={8} />
+                        <Text style={{ fontFamily: 'Font-Title'}} fontSize='4xl'
+                        >DEFEAT</Text>
+                        <Image src={require('../assets/fight_gray.png')} w={8} h={8} />
+                    </Flex>
+                </Flex>
+                <Flex direction='column'>
+                    
+                </Flex>
+            </Flex>
+        </Center>
+    );
+};
 
 const GameOver: React.FC = () => {
     const location = useLocation();
@@ -8,8 +40,8 @@ const GameOver: React.FC = () => {
     return (
       <div>
         {win ? 
-        (<h1>Congratulations! You won!</h1>) : 
-        (<h1>Game Over! Better luck next time!</h1>)}
+        <GameOverWin /> : 
+        <GameOverLose />}
       </div>
     );
   };
