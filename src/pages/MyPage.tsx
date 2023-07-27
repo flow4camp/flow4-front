a60f1131810250e0a89c5f3c618f711dfc1c6dad
YinaePark <dlsoso1@unist.ac.kr>
2023-07-27 17:59:41 +0900

    feat: 유저이름, 유령이름 연동

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -12,8 +12,17 @@ import React, { useState } from "react";
 import Layout from "../components/layout";
 import { Link } from "react-router-dom";
 import UserCharacter from "../components/UserCharacter";
-import { accVariants, clothesVariants, faceVariants, hatVariants, shoeVariants } from "../components/AssetVariants";
-import { useUserContext } from '../context/UserContext';
+import {
+  accVariants,
+  clothesVariants,
+  faceVariants,
+  hatVariants,
+  shoeVariants,
+} from "../components/AssetVariants";
+import { useUserContext, User } from "../context/UserContext";
+import { API_URL } from "../api";
+import axios from "axios";
+
 import { ChevronRightIcon } from "@chakra-ui/icons";
 
 function MyPage() {
@@ -33,15 +42,32 @@ function MyPage() {
     setInputValueGhostName(ghostName);
   };
 
-  const handleSaveClick = () => {
-    setIsEditing(false);
-    setName(inputValue);
-    setIsEditingGhostName(false);
-    setGhostName(inputValueGhostName);
+  const handleSaveClick = async () => {
+    try {
+      setIsEditing(false);
+      setIsEditingGhostName(false);
+
+      await axios.put(`${API_URL}/user/${user.id}/username`, {
+        username: inputValue,
+      });
+      const response = await axios.put(`${API_URL}/user/${user.id}/ghostname`, {
+        ghostname: inputValueGhostName,
+      });
+      console.log(response.data);
+      console.log("테스트테스트");
+
+      setUser((prevUser: User) => ({
+        ...prevUser,
+        username: response.data.username,
+        ghostname: response.data.ghostname,
+      }));
+    } catch (error) {
+      console.error("Error updating username and ghostname:", error);
+    }
   };
 
-  const firstBorderColor = theme.colors.line_1_color;
-  const secondBorderColor = theme.colors.line_2_color;
+  const firstBorderColor = theme.colors[`line_${user.subwayNum1}_color`];
+  const secondBorderColor = theme.colors[`line_${user.subwayNum2}_color`];
 
   return (
     <Layout>
@@ -56,7 +82,7 @@ function MyPage() {
           마이페이지
         </Text>
       </Box>
-      {user &&
+      {user && (
         <Flex
           direction="column"
           justify="center"
@@ -77,11 +103,27 @@ function MyPage() {
             >
               <UserCharacter
                 usage={"profile"}
-                selectedHat={user.hatVariants !== -1 ? hatVariants[user.hatVariants] : null}
-                selectedAcc={user.accVariants !== -1 ? accVariants[user.accVariants] : null}
-                selectedFace={user.faceVariants !== -1 ? faceVariants[user.faceVariants] : faceVariants[0]}
-                selectedClothes={user.clothesVariants !== -1 ? clothesVariants[user.clothesVariants] : null}
-                selectedShoe={user.shoeVariants !== -1 ? shoeVariants[user.shoeVariants] : null}
+                selectedHat={
+                  user.hatVariants !== -1 ? hatVariants[user.hatVariants] : null
+                }
+                selectedAcc={
+                  user.accVariants !== -1 ? accVariants[user.accVariants] : null
+                }
+                selectedFace={
+                  user.faceVariants !== -1
+                    ? faceVariants[user.faceVariants]
+                    : faceVariants[0]
+                }
+                selectedClothes={
+                  user.clothesVariants !== -1
+                    ? clothesVariants[user.clothesVariants]
+                    : null
+                }
+                selectedShoe={
+                  user.shoeVariants !== -1
+                    ? shoeVariants[user.shoeVariants]
+                    : null
+                }
               />
             </Flex>
             <Flex direction="column" style={{ marginTop: "20px" }}>
@@ -98,18 +140,24 @@ function MyPage() {
                   style={{ fontFamily: "Font-Title", alignSelf: "center" }}
                   onClick={handleEditClick}
                 >
-                  {ghostName}
+                  {user.ghostname}
                 </Text>
               )}
               {isEditing ? (
                 <Input
                   value={inputValue}
                   onChange={(e) => setInputValue(e.target.value)}
-                  style={{ fontFamily: "Font-Title-light", alignSelf: "center" }}
+                  style={{
+                    fontFamily: "Font-Title-light",
+                    alignSelf: "center",
+                  }}
                 />
               ) : (
                 <Text
-                  style={{ fontFamily: "Font-Title-light", alignSelf: "center" }}
+                  style={{
+                    fontFamily: "Font-Title-light",
+                    alignSelf: "center",
+                  }}
                   onClick={handleEditClick}
                 >
                   {user.username}
@@ -132,7 +180,9 @@ function MyPage() {
                   h="20px"
                   style={{ marginRight: "10px" }}
                 />
-                <Text style={{ fontFamily: "Font-content" }}>{`전적 ${user.win}승 ${user.lose}패`}</Text>
+                <Text
+                  style={{ fontFamily: "Font-content" }}
+                >{`전적 ${user.win}승 ${user.lose}패`}</Text>
               </Flex>
               <Flex>
                 <Image
@@ -141,108 +191,108 @@ function MyPage() {
                   h="25px"
                   style={{ marginRight: "5px" }}
                 />
-                <Text style={{ fontFamily: "Font-content" }}>{`기력 ${user.power}`}</Text>
+                <Text
+                  style={{ fontFamily: "Font-content" }}
+                >{`기력 ${user.power}`}</Text>
               </Flex>
             </Flex>
             <Flex
-                as={Link}
-                to="/edit-metroline"
-                align="center"
-                justify="center"
-                style={{
-                  borderRadius: "20px",
-                  height: "150px",
-                  width: "80%",
-                  marginTop: "20px",
-                  backgroundColor: theme.colors.ziha_charcoal,
-                  position: "relative",
-                }}
-              >
-                {/* 첫번째 역 */}
-                <Flex direction="column" align="center">
-                  <div
-                    style={{
-                      width: "15px",
-                      height: "15px",
-                      border: "3px solid #ccc",
-                      borderRadius: "50%",
-                      backgroundColor: "white",
-                    }}
-                  ></div>
-                  <Text
-                    style={{
-                      fontFamily: "Font-title-light",
-                      color: "white",
-                      fontSize: "10px",
-                    }}
-                  >
-                    신설동
-                  </Text>
-                </Flex>
-
-                <Box
+              as={Link}
+              to="/edit-metroline"
+              align="center"
+              justify="center"
+              style={{
+                borderRadius: "20px",
+                height: "150px",
+                width: "80%",
+                marginTop: "20px",
+                backgroundColor: theme.colors.ziha_charcoal,
+                position: "relative",
+              }}
+            >
+              {/* 첫번째 역 */}
+              <Flex direction="column" align="center">
+                <div
                   style={{
-                    width: "20%",
-                    height: "0",
-                    border: `2px solid ${firstBorderColor}`,
-                    borderRadius: "20px",
-                    top: "50%",
+                    width: "15px",
+                    height: "15px",
+                    border: "3px solid #ccc",
+                    borderRadius: "50%",
+                    backgroundColor: "white",
                   }}
-                ></Box>
-                {/* 두번째 역 */}
-                <Flex direction="column" align="center">
-                  <div
-                    style={{
-                      width: "15px",
-                      height: "15px",
-                      border: "3px solid #ccc",
-                      borderRadius: "50%",
-                      backgroundColor: "white",
-                    }}
-                  ></div>
-                  <Text
-                    style={{
-                      fontFamily: "Font-title-light",
-                      color: "white",
-                      fontSize: "10px",
-                    }}
-                  >
-                    시청역
-                  </Text>
-                </Flex>
-                <Box
+                ></div>
+                <Text
                   style={{
-                    width: "20%",
-                    height: "0",
-                    border: `2px solid ${secondBorderColor}`,
-                    borderRadius: "20px",
-                    top: "50%",
+                    fontFamily: "Font-title-light",
+                    color: "white",
+                    fontSize: "10px",
                   }}
-                ></Box>
-                {/* 세번째 역 */}
-                <Flex direction="column" align="center">
-                  <div
-                    style={{
-                      width: "15px",
-                      height: "15px",
-                      border: "3px solid #ccc",
-                      borderRadius: "50%", // 원형으로 만들기 위해 50%의 borderRadius 사용
-                      backgroundColor: "white", // 예시로 파란색 배경을 적용
-                    }}
-                  ></div>
-                  <Text
-                    style={{
-                      fontFamily: "Font-title-light",
-                      color: "white",
-                      fontSize: "10px",
-                    }}
-                  >
-                    합정역
-                  </Text>
-                </Flex>
+                >
+                  {user.station1}
+                </Text>
               </Flex>
 
-
+              <Box
+                style={{
+                  width: "20%",
+                  height: "0",
+                  border: `2px solid ${firstBorderColor}`,
+                  borderRadius: "20px",
+                  top: "50%",
+                }}
+              ></Box>
+              {/* 두번째 역 */}
+              <Flex direction="column" align="center">
+                <div
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    border: "3px solid #ccc",
+                    borderRadius: "50%",
+                    backgroundColor: "white",
+                  }}
+                ></div>
+                <Text
+                  style={{
+                    fontFamily: "Font-title-light",
+                    color: "white",
+                    fontSize: "10px",
+                  }}
+                >
+                  {user.station2}
+                </Text>
+              </Flex>
+              <Box
+                style={{
+                  width: "20%",
+                  height: "0",
+                  border: `2px solid ${secondBorderColor}`,
+                  borderRadius: "20px",
+                  top: "50%",
+                }}
+              ></Box>
+              {/* 세번째 역 */}
+              <Flex direction="column" align="center">
+                <div
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    border: "3px solid #ccc",
+                    borderRadius: "50%", // 원형으로 만들기 위해 50%의 borderRadius 사용
+                    backgroundColor: "white", // 예시로 파란색 배경을 적용
+                  }}
+                ></div>
+                <Text
+                  style={{
+                    fontFamily: "Font-title-light",
+                    color: "white",
+                    fontSize: "10px",
+                  }}
+                >
+                  {user.station3}
+                </Text>
+              </Flex>
+            </Flex>
           </Flex>
           <Flex direction="column" style={{ width: "80%", marginTop: "50px" }}>
             <Button
@@ -278,7 +328,7 @@ function MyPage() {
             </Button>
           </Flex>
         </Flex>
-      }
+      )}
     </Layout>
   );
 }
