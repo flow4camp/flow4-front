87c58580f2d696ef28d641ceace8ea23c65e2e1a
YinaePark <dlsoso1@unist.ac.kr>
2023-07-27 19:32:58 +0900

    feat: 기력충전소

--- src/pages/StaminaStore.tsx
+++ src/pages/StaminaStore.tsx
@@ -10,6 +10,7 @@ import {
 } from "@chakra-ui/react";
 
 function StaminaStore() {
+  const theme = useTheme();
   return (
     <Layout>
       <Box w="100%">
@@ -23,6 +24,138 @@ function StaminaStore() {
           기력 충전
         </Text>
       </Box>
+      <Flex justify="center" align="center" paddingTop="50px">
+        <Flex
+          justify="center"
+          align="center"
+          w="350px"
+          h="200px"
+          style={{
+            backgroundColor: theme.colors.ziha_charcoal,
+            borderRadius: "30PX",
+          }}
+        >
+          <Flex direction="column" m={3}>
+            <Text
+              w="fit-content"
+              fontSize="lg"
+              style={{ fontFamily: "Font-Title", color: "white" }}
+            >
+              직장인 패키지
+            </Text>
+            <Flex align="center">
+              <Text
+                w="fit-content"
+                fontSize="lg"
+                style={{
+                  fontFamily: "Font-Title-light",
+                  color: "white",
+                  textDecoration: "line-through",
+                }}
+              >
+                19,990
+              </Text>
+              <Text
+                w="fit-content"
+                fontSize="lg"
+                marginLeft="10px"
+                style={{
+                  fontFamily: "Font-Title-light",
+                  color: "red",
+                }}
+              >
+                → 9,990
+              </Text>
+            </Flex>
+            <Text
+              marginTop="20px"
+              w="fit-content"
+              fontSize="13px"
+              style={{ fontFamily: "Font-Title-light", color: "white" }}
+            >
+              500 기력
+            </Text>
+            <Text
+              w="fit-content"
+              fontSize="13px"
+              style={{ fontFamily: "Font-Title-light", color: "white" }}
+            >
+              유령 정장 + 유령 넥타이
+            </Text>
+          </Flex>
+          <Image
+            w="150px"
+            h="150px"
+            src={require("../assets/tired_ghost.png")}
+          ></Image>
+        </Flex>
+      </Flex>
+      <Flex justify="center" align="center" paddingTop="50px">
+        <Flex
+          justify="center"
+          align="center"
+          w="350px"
+          h="200px"
+          style={{
+            backgroundColor: theme.colors.ziha_charcoal,
+            borderRadius: "30PX",
+          }}
+        >
+          <Flex direction="column" m={3}>
+            <Text
+              w="fit-content"
+              fontSize="lg"
+              style={{ fontFamily: "Font-Title", color: "white" }}
+            >
+              실속 패키지
+            </Text>
+            <Flex align="center">
+              <Text
+                w="fit-content"
+                fontSize="lg"
+                style={{
+                  fontFamily: "Font-Title-light",
+                  color: "white",
+                  textDecoration: "line-through",
+                }}
+              >
+                49,990
+              </Text>
+              <Text
+                w="fit-content"
+                fontSize="lg"
+                marginLeft="10px"
+                style={{
+                  fontFamily: "Font-Title-light",
+                  color: "red",
+                }}
+              >
+                → 25,000
+              </Text>
+            </Flex>
+            <Text
+              marginTop="20px"
+              w="fit-content"
+              fontSize="13px"
+              style={{ fontFamily: "Font-Title-light", color: "white" }}
+            >
+              5000 기력
+            </Text>
+            <Text
+              w="fit-content"
+              fontSize="13px"
+              style={{ fontFamily: "Font-Title-light", color: "white" }}
+            >
+              아이템 10+1 랜덤박스
+            </Text>
+          </Flex>
+          <Image
+            w="120px"
+            h="120px"
+            src={require("../assets/stamina_chest.png")}
+          ></Image>
+        </Flex>
+      </Flex>
     </Layout>
   );
 }
